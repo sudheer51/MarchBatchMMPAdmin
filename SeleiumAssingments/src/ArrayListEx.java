45075f99bb2b20c97c90770e4586d554e8022a07
sudheer <sudheer694@gmail.com>
2018-04-13 04:58:07 +0530

    Pushing the code into Github for the first time into master branch.

---
+++ SeleiumAssingments/src/ArrayListEx.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+import org.openqa.selenium.By;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		
+		 	
+		/**
+		 * What are generics in java??
+		 * generics enable types (classes and interfaces) to be parameters when defining classes,
+		 *  interfaces and methods. ... Stronger type checks at compile time
+		 * List is an interface useful to store duplicate as-well-as unique object
+		 */
+		List<Integer> aList = new Vector<Integer>();
+		//add,size,get
+		 
+		aList.add(5);
+		aList.add(6);
+		aList.add(7);
+		aList.add(7);
+
+		aList.iterator();
+		System.out.println("size" + aList.size());
+		
+	 
+		aList.remove(0);
+		System.out.println("size" + aList.size());
+		
+		for(int i =0 ;i <aList.size();i++)
+		{
+			System.out.println(aList.get(i));
+		}
+	}
+}
---
+++ SeleiumAssingments/src/AutoSuggest.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoSuggest {
+	
+	static WebDriver driver;
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://in.yahoo.com/?p=us");
+		driver.findElement(By.id("uh-search-box")).sendKeys("Selenium WebDriver");
+		Thread.sleep(5000);
+		
+		List<WebElement> ulList = driver.findElements(By.cssSelector("ul[role='listbox']>li"));
+		System.out.println("number of children under ul "+ ulList.size());
+		
+		for(int i =0 ;i < ulList.size();i++)
+		{
+			if(ulList.get(i).getText().contains("interview"))
+			{
+				System.out.println("Match Found!!!");
+				ulList.get(i).click();
+				break;
+			}
+		}
+	}
+
+}
